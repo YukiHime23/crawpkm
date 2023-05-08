d338fc7223528a01e4fe16956bf2b2166dddaabd
YukiHime23 <luutruhocit@gmail.com>
2023-05-09 00:49:07 +0700

    add docker

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"goCraw/controller"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
 	crawCtrl := controller.NewCrawController()
+	router.GET("/check-heath", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Oke",
+		})
+	})
 	router.GET("/craw-data/pokemon-special", crawCtrl.CrawPkmSpecial)
 	router.GET("/wallpaper/azur-lane", crawCtrl.CrawAzurLaneWallpaper)
 
-	router.Run(":8181")
+	router.Run(":8080")
 }
