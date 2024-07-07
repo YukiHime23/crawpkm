d6b392f71cb2f30b62b18dca8d3590efe5f06bd2
YukiHime23 <luutruhocit@gmail.com>
2024-07-08 00:59:28 +0700

    update

--- cmd/pkmcraw/main.go
+++ cmd/pkmcraw/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"strings"
 
-	"github.com/YukiHime23/crawpkm"
 	"github.com/gocolly/colly"
 )
 
@@ -32,10 +30,12 @@ func main() {
 	)
 
 	var chap Chapter
+
+	mapChap := make(map[string][]string)
 	checkEnd := false
 
-	nextVolume := "https://www.pokemonspecial.com/2014/06/chapter-021.html"
-	// nextVolume := LinkPkmCraw
+	BaseURL := "https://www.pokemonspecial.com/2014/06/chapter-003.html"
+	nextVolume := LinkPkmCraw
 
 	for {
 		collectorPKM.OnHTML("h3.post-title.entry-title", func(e *colly.HTMLElement) {
@@ -45,25 +45,18 @@ func main() {
 		collectorPKM.OnHTML(".separator a", func(element *colly.HTMLElement) {
 			link := element.Attr("href")
 			chap.PageLink = append(chap.PageLink, link)
+
+			mapChap = map[string][]string{
+				chap.Title: chap.PageLink,
+			}
 		})
 
-		collectorPKM.OnHTML("a#Blog1_blog-pager-newer-link.blog-pager-newer-link", func(element *colly.HTMLElement) {
+		collectorPKM.OnHTML("a#Blog1_blog-pager-newer-link", func(element *colly.HTMLElement) {
 			nextVolume = element.Attr("href")
 			if nextVolume == BaseURL {
 				checkEnd = true
 			}
 		})
-
-		if err := os.MkdirAll(PathPkm+chap.Title, os.ModePerm); err != nil {
-			log.Fatal(err)
-		}
-
-		for _, v := range chap.PageLink {
-			if err := crawpkm.DownloadFile(v, "", PathPkm+chap.Title); err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("craw -> " + chap.Title + " <- done!")
-		}
 
 		if err := collectorPKM.Visit(nextVolume); err != nil {
 			log.Fatal(err)
@@ -71,6 +64,25 @@ func main() {
 
 		if checkEnd {
 			break
+		}
+	}
+
+	for k, v := range mapChap {
+		// if err := os.MkdirAll(PathPkm+k, os.ModePerm); err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		// for _, x := range v {
+		// 	if err := crawpkm.DownloadFile(x, "", PathPkm+k); err != nil {
+		// 		log.Fatal(err)
+		// 	}
+		// 	fmt.Println("craw -> " + k + " <- done!")
+		// }
+
+		fmt.Println(k)
+
+		for _, x := range v {
+			fmt.Println(x)
 		}
 	}
 }
