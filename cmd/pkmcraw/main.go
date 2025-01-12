43d0be2df1f4c2e6731591f39414febd93b691a7
YukiHime23 <luutruhocit@gmail.com>
2025-01-13 01:29:23 +0700

    update

--- cmd/pkmcraw/main.go
+++ cmd/pkmcraw/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/YukiHime23/crawpkm"
 	"github.com/gocolly/colly"
 )
 
@@ -30,47 +31,36 @@ var collectorPKM = colly.NewCollector(
 )
 
 func readHTML() map[string][]string {
-
 	var title string
-	var pages []string
 	mapChap := make(map[string][]string)
-	checkEnd := false
 
-	BaseURL := "https://www.pokemonspecial.com/2014/06/chapter-003.html"
-	nextVolume := LinkPkmCraw
+	collectorPKM.OnHTML("a#Blog1_blog-pager-newer-link", func(element *colly.HTMLElement) {
+		nextVolume := element.Attr("href")
+		fmt.Println(nextVolume)
+		collectorPKM.Visit(element.Request.AbsoluteURL(nextVolume))
+	})
 
-	for {
-		collectorPKM.OnHTML("h3.post-title", func(element *colly.HTMLElement) {
-			title = strings.Trim(element.Text, "\n")
-		})
+	collectorPKM.OnHTML("h3.post-title", func(element *colly.HTMLElement) {
+		title = strings.Trim(element.Text, "\n")
+	})
 
-		// collectorPKM.OnHTML(".separator a", func(element *colly.HTMLElement) {
-		// 	link := element.Attr("href")
-		// 	pages = append(pages, link)
-		// 	mapChap[title] = pages
-		// })
+	collectorPKM.OnHTML(".separator a", func(element *colly.HTMLElement) {
+		link := element.Attr("href")
+		mapChap[title] = append(mapChap[title], link)
+	})
 
-		collectorPKM.OnHTML(".post-body div a", func(element *colly.HTMLElement) {
-			link := element.Attr("href")
-			pages = append(pages, link)
-			mapChap[title] = pages
-		})
+	// collectorPKM.OnHTML(".post-body div a", func(element *colly.HTMLElement) {
+	// 	link := element.Attr("href")
+	// 	pages = append(pages, link)
+	// 	mapChap[title] = pages
+	// })
 
-		collectorPKM.OnHTML("a#Blog1_blog-pager-newer-link", func(element *colly.HTMLElement) {
-			nextVolume = element.Attr("href")
-			if nextVolume == BaseURL {
-				checkEnd = true
-			}
-		})
-
-		if err := collectorPKM.Visit(nextVolume); err != nil {
-			log.Fatal(err)
-		}
-
-		if checkEnd {
-			break
-		}
+	if err := collectorPKM.Visit(LinkPkmCraw); err != nil {
+		log.Fatal(err)
 	}
+
+	// Wait until threads are finished
+	collectorPKM.Wait()
 
 	return mapChap
 }
@@ -83,12 +73,10 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Println(k)
-		fmt.Println(len(v))
-		// for _, x := range v {
-		// if err := crawpkm.DownloadFile(x, "", PathPkm+k); err != nil {
-		// 	log.Fatal(err)
-		// }
-		// }
+		for _, x := range v {
+			if err := crawpkm.DownloadFile(x, "", PathPkm+k); err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
